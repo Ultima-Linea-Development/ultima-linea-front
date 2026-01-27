b30f7fc75bd146cf6da0dd706a16d4b5c2ea57ff
Renzo <nviscio@gmail.com>
2026-01-26 23:06:25 -0300

    next config standaole

--- next.config.ts
+++ next.config.ts
@@ -2,22 +2,25 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
+
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "photo.yupoo.com",
-      },
+      { protocol: "https", hostname: "photo.yupoo.com" },
     ],
     unoptimized: true,
   },
+
   turbopack: {
     root: path.resolve(__dirname),
   },
+
   transpilePackages: ["react-icons"],
+
   async rewrites() {
-    const apiUrl = process.env.NEXT_PUBLIC_API_URL || "https://api.ultimalinea.com.ar";
-    
+    const apiUrl =
+      process.env.NEXT_PUBLIC_API_URL || "https://api.ultimalinea.com.ar";
+
     if (process.env.NODE_ENV === "development") {
       return [
         {
@@ -26,7 +29,7 @@ const nextConfig: NextConfig = {
         },
       ];
     }
-    
+
     return [];
   },
 };
